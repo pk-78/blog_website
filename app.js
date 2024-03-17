779e885cad99f1177f9953ef68aa07c8f80a73cc
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-03-18 02:19:27 +0530

    "Database added"

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect("mongodb://127.0.0.1:27017/blog");
+
+const homeStartingContent = "Welcome to Priyanshu Kushwaha's Blog! I'm Priyanshu, a 3rd-year engineering student at Rajkiya Engineering College Sonbhadra. Here, I share daily insights into my routine, study tips for excelling academically, and reflections on personal growth. Expect a glimpse into my daily life, from early study sessions to late-night coding, as I navigate the challenges of engineering coursework. I'll share effective study strategies to help you succeed, drawing from my own experiences. Beyond academics, I'm passionate about personal growth and self-improvement, and I'll offer actionable advice and reflections on my journey. Community engagement is vital to me, so join the conversation in the comments and connect with fellow students and growth enthusiasts. Whether you're an engineering student or simply interested in personal development, my blog offers a space to learn, grow, and strive for excellence together. Thanks for joining me on this adventure!";
+const aboutContent = "Welcome to Priyanshu Kushwaha's Blog, your hub for engineering insights and personal growth inspiration! I'm Priyanshu Kushwaha, a dedicated 3rd-year engineering student at Rajkiya Engineering College Sonbhadra. Here, I invite you to explore my world—a blend of academic rigor and personal development journey.This blog is a chronicle of my daily life, filled with the challenges and triumphs of navigating the complexities of engineering studies. From early morning lectures to late-night study sessions, I share firsthand experiences and practical tips that have helped me excel academically.But this space is more than just study hacks. It's a reflection of my commitment to personal growth and self-improvement. Through candid reflections and actionable insights, I aim to inspire and empower you to embark on your own journey of growth.Why should you stick around? Whether you're an engineering student seeking study strategies, an aspiring individual hungry for personal development, or simply curious about life at Rajkiya Engineering College Sonbhadra, this blog offers valuable content tailored to your interests.Join our community of learners and growth enthusiasts! Engage with the content, share your thoughts in the comments, and connect with like-minded individuals who are passionate about learning and growth.Thank you for visiting Priyanshu Kushwaha's Blog. Together, let's embark on a journey of discovery, learning, and growth. Here's to unlocking our fullest potential, one blog post at a time!Warm regards,Priyanshu Kushwaha"
+
+
+const contactContent =" Contact Priyanshu Kushwaha:"
+
+
 
 const app = express();
 
@@ -17,11 +24,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-let composeItems=[]
+// let composeItems=[]
+
+
+const blogSchema ={
+  title: String,
+  post: String
+}
+
+const Blog =mongoose.model("Blog", blogSchema);
+
 
 app.get("/", function(req,res){
   // res.send("<h1>Home</h1>");
-  res.render("home",{homefirstcontent:homeStartingContent,addedcompose:composeItems})
+Blog.find().then(function(result){
+  
+    res.render("home",{homefirstcontent:homeStartingContent,addedcompose:result})
+  
+}).catch(function(err){
+  console.log(err);
+});
+
+  
 
 });
 
@@ -44,27 +68,53 @@ app.get("/contact",function(req,res){
 });
 
 app.get("/compose",function(req,res){
-  // res.sendFile(__dirname+"/views/compose.ejs")
+
+  
+  
+
+
+
   
   res.render("compose");
 });
 
 app.post("/compose",function(req,res){
-  const composeItem={
-    titleContent:req.body.inputTitle,
-    postContent:req.body.inputPost
-  }
   
-  composeItems.push(composeItem);
-  console.log(composeItems)
+    titleContent=req.body.inputTitle,
+    postContent=req.body.inputPost
+  
+  
+  
+  console.log(titleContent);
+  console.log(postContent);
   // console.log(composeItems[0].titleContent)
+
+
+
+
+  const blog = new Blog({
+    title: titleContent,
+    post: postContent    
+    
+  })
+
+  blog.save()
+  .then(() => {
+      res.redirect("/");
+  })
+  .catch((err) => {
+      // Handle error if needed
+  });
+
+    
+    
   
   
   res.redirect("/");
 
 })
 
-app.get("/post/:postType",function(req,res){
+app.get("/post/:postId",function(req,res){
   // for(let i=0;i<composeItems.length;i++){
   //   if(req.params.postType===composeItems[i].titleContent){
   //     console.log("Matching");
@@ -78,21 +128,30 @@ app.get("/post/:postType",function(req,res){
     
   
   // }
+  
+  const requestedPostId = req.params.postId;
+  // console.log(requestedPostId);
 
-  let afterPostHeaading = _.lowerCase(req.params.postType)
+  
+  
 
+  Blog.findOne({_id: requestedPostId})
+    .then(blog => {
+        res.render("post", {singleTitle: blog.title, singlePost: blog.post });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+  // Blog._id.forEach(function(element){
+  //   let postHeading=_.lowerCase(element.title)
+  //   if(postHeading===afterPostHeaading){
 
-
-  composeItems.forEach(function(element){
-    let postHeading=_.lowerCase(element.titleContent)
-    if(postHeading===afterPostHeaading){
-
-      res.render("post",{singlePostHeading:element.titleContent,singlePostContent:element.postContent})
-    }
-    else {
-      console.log("Not Matching");
-    }
-  });
+  //     res.render("post",{singlePostHeading:element.title,singlePostContent:element.post})
+  //   }
+  //   else {
+  //     console.log("Not Matching");
+  //   }
+  // });
   
   
 
